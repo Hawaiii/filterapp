af19ac8b27aff8c7472e8a333da0a31432938740
iszfan <zhifan.li42@gmail.com>
2015-01-31 10:38:03 -0600

    android app:

---
+++ android/Filter/src/com/example/filter/Camera.java
@@ -0,0 +1,51 @@
+package com.example.filter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Camera extends Activity {
+
+	 Button btnCamera;
+	 Button btnUpload;
+	 ImageView imgFavorite;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        //open the camera
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+
+        imgFavorite = (ImageView)findViewById(R.id.imageView1);
+
+    }
+
+
+     @Override
+     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        imgFavorite.setImageBitmap(bp);
+     }
+    
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+   
+    
+}
