a9fd51cb7b624aa97f1b409431f312315a0fed59
hawaiii <yihua25@gmail.com>
2015-01-31 21:21:16 -0600

    refactored matrix multiplications

--- android/Filter/src/com/example/filter/improc/ImgProcessor.java
+++ android/Filter/src/com/example/filter/improc/ImgProcessor.java
@@ -16,6 +16,18 @@ public class ImgProcessor {
 	private static double[][] M = new double[][]{{0.5141, 0.3239, 0.1604},
 			{0.2651,0.6702,0.0641},
 			{0.0241,0.1228,0.8444}};
+	private static double[][] L = new double[][]{{0.3897,0.6890,-0.0787},
+		{-0.2298,1.1834,0.0464},
+		{0.0000,0.0000,1.0000}};
+	private static double[][] LMinv = {{4.46794,-3.58732,0.119314},
+		{-1.21858, 2.3809, -0.162388},
+		{0.0496975, -0.243866, 1.20448}};
+	private static double[][] AB = new double[][]{{0.5774,0.5774,0.5774},
+		{0.4082,0.4082,-0.8165},
+		{0.7071,-0.7071,0}};
+	private static double[][] ABinv = new double[][]{{1/Math.sqrt(3),0,0},
+		{0,1/Math.sqrt(6),0},
+		{0,0,0}};
 	
 	public ImgProcessor(){
 		savedFilters = new HashMap<String, Filter>();
@@ -212,33 +224,10 @@ public class ImgProcessor {
 	}
 	
 	private double[][][] xyz2lms(double[][][]xyz){
-//		if (xyz == null || xyz[0] == null || 
-//				xyz[0][0] == null || xyz[0][0].length != 3){
-//			System.out.println("bad dimensions for xyz2lms!");
-//			return null;
-//		}		
-		double[][] L = new double[][]{{0.3897,0.6890,-0.0787},
-				{-0.2298,1.1834,0.0464},
-				{0.0000,0.0000,1.0000}};
-
-//		int width = xyz.length;
-//		int height = xyz[0].length;
-//		double[][][]lms = new double[width][height][3];
-//		for (int x = 0; x < width; ++x){
-//			for (int y = 0; y < height; ++y){
-//				lms[x][y][0] = L[0][0]*xyz[x][y][0] + L[0][1]*xyz[x][y][1] + L[0][2]*xyz[x][y][2];
-//				lms[x][y][1] = L[1][0]*xyz[x][y][0] + L[1][1]*xyz[x][y][1] + L[1][2]*xyz[x][y][2];
-//				lms[x][y][2] = L[2][0]*xyz[x][y][0] + L[2][1]*xyz[x][y][1] + L[2][2]*xyz[x][y][2];
-//			}
-//		}
-//		xyz = null; // releases the original array?
 		return matrixMultiply(L, xyz);
 	}
 	
 	private double[][][] lms2rgb(double[][][] lms){
-		double[][] LMinv = {{4.46794,-3.58732,0.119314},
-				{-1.21858, 2.3809, -0.162388},
-				{0.0496975, -0.243866, 1.20448}};
 		return matrixMultiply(LMinv, lms);
 	}
 	
@@ -283,30 +272,10 @@ public class ImgProcessor {
 	}
 	
 	private double[][][] lms2lab(double[][][] llms){
-		
-		double[][] AB = new double[][]{{0.5774,0.5774,0.5774},
-				{0.4082,0.4082,-0.8165},
-				{0.7071,-0.7071,0}};
-		
-//		int width = llms.length;
-//		int height = llms[0].length;
-//		
-//		double[][][]lab = new double[width][height][3];
-//		for (int x = 0; x < width; ++x){
-//			for (int y = 0; y < height; ++y){
-//				lab[x][y][0] = AB[0][0]*llms[x][y][0] + AB[0][1]*llms[x][y][1] + AB[0][2]*llms[x][y][2];
-//				lab[x][y][1] = AB[1][0]*llms[x][y][0] + AB[1][1]*llms[x][y][1] + AB[1][2]*llms[x][y][2];
-//				lab[x][y][2] = AB[2][0]*llms[x][y][0] + AB[2][1]*llms[x][y][1] + AB[2][2]*llms[x][y][2];
-//			}
-//		}
-//		llms = null; // release llms?
 		return matrixMultiply(AB, llms);
 	}
 	
 	private double[][][] lab2llms(double [][][] lab){
-		double[][] ABinv = new double[][]{{1/Math.sqrt(3),0,0},
-				{0,1/Math.sqrt(6),0},
-				{0,0,0}};
 		return matrixMultiply(ABinv, lab);
 	}
 	
@@ -329,7 +298,7 @@ public class ImgProcessor {
 				res[x][y][2] = small[2][0]*big[x][y][0] + small[2][1]*big[x][y][1] + small[2][2]*big[x][y][2];
 			}
 		}
-		big = null; // release llms?
+		big = null; // release big array
 		return res;
 	}
 
